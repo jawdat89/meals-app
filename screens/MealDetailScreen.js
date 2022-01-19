0cc3bc237e06e856fda047d503ec9c8d4ec553c9
Jawdat <Jawdat.89@gmail.com>
2022-01-19 18:11:28 +0200

    icon header

--- screens/MealDetailScreen.js
+++ screens/MealDetailScreen.js
@@ -1,12 +1,15 @@
-import React from 'react';
-import { View, Text, Button, StyleSheet } from 'react-native';
+import React from "react";
+import { View, Text, Button, StyleSheet } from "react-native";
+import { HeaderButtons, Item } from "react-navigation-header-buttons";
 
-import { MEALS } from '../data/dummy-data';
+import { MEALS } from "../data/dummy-data";
 
-const MealDetailScreen = props => {
-  const mealId = props.navigation.getParam('mealId');
+import HeaderButton from '../components/HeaderButton';
 
-  const selectedMeal = MEALS.find(meal => meal.id === mealId);
+const MealDetailScreen = (props) => {
+  const mealId = props.navigation.getParam("mealId");
+
+  const selectedMeal = MEALS.find((meal) => meal.id === mealId);
 
   return (
     <View style={styles.screen}>
@@ -22,20 +25,25 @@ const MealDetailScreen = props => {
 };
 
 MealDetailScreen.navigationOptions = (navigationData) => {
-  const mealId = navigationData.navigation.getParam('mealId');
-  const selectedMeal = MEALS.find(meal => meal.id === mealId);
+  const mealId = navigationData.navigation.getParam("mealId");
+  const selectedMeal = MEALS.find((meal) => meal.id === mealId);
 
   return {
-    headerTitle: selectedMeal.title
+    headerTitle: selectedMeal.title,
+    headerRight: (
+      <HeaderButtons HeaderButtonComponent={HeaderButton}>
+        <Item title="Favorite" iconName="ios-star" />
+      </HeaderButtons>
+    ),
   };
-}
+};
 
 const styles = StyleSheet.create({
   screen: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center'
-  }
+    justifyContent: "center",
+    alignItems: "center",
+  },
 });
 
 export default MealDetailScreen;
